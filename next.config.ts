e595c591b939423100df8df39c8e016498983eda
Roi Krispin <roik@Rois-MacBook-Air.local>
2026-03-16 13:16:21 +0200

    Revert "evolver: merge evolver/idea-fb4f739c"
    This reverts commit f923ad7f4256f47c2f757b7606fdb7034d49a7c5, reversing
    changes made to fd05518de49bcecba66ef7ffca589af6f7f9ea0c.

--- next.config.ts
+++ next.config.ts
@@ -4,52 +4,6 @@ import { withSentryConfig } from "@sentry/nextjs";
 const nextConfig: NextConfig = {
   // Enable Turbopack support (Next.js 16)
   turbopack: {},
-  
-  // Bundle optimization
-  experimental: {
-    optimizePackageImports: [
-      'lucide-react',
-      '@vercel/analytics',
-      'react-markdown',
-      'recharts',
-    ],
-  },
-  
-  // Compression and optimization
-  compress: true,
-  poweredByHeader: false,
-  
-  // Tree shaking for smaller bundles
-  swcMinify: true,
-  
-  webpack: (config, { isServer }) => {
-    // Enable tree shaking
-    config.optimization.providedExports = true;
-    config.optimization.usedExports = true;
-    config.optimization.sideEffects = false;
-    
-    // Split chunks for better caching
-    if (!isServer) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all',
-          },
-          common: {
-            name: 'common',
-            minChunks: 2,
-            chunks: 'all',
-            enforce: true,
-          },
-        },
-      };
-    }
-    
-    return config;
-  },
 };
 
 // @ts-ignore - Sentry v7 types don't fully support Next.js 16 yet
